d948a30a425562650bd08ee89908053530f40c51
scyanth <34488277+scyanth@users.noreply.github.com>
2024-09-30 11:55:31 +0200

    quete03

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,30 @@
+const pokemonList = [
+
+    {
+  
+      name: "bulbasaur",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  
+    },
+  
+    {
+  
+      name: "mew",
+  
+    },
+  
+  ];
+
 function PokemonCard() {
 
+    const pokemon = pokemonList[0];
+
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc}></img> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
         </figure>;
   
 }
