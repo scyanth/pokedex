3c5b36bc6fe6514dfe985f94a39bf464e30840ea
scyanth <34488277+scyanth@users.noreply.github.com>
2024-09-30 12:19:04 +0200

    quete04

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
 import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+
+  {
+
+    name: "bulbasaur",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+  },
+
+  {
+
+    name: "mew",
+
+  },
+
+];
+
 function App() {
   
   return (
-    <div><PokemonCard/></div>
+    <div><PokemonCard pokemon={pokemonList[0]}/></div>
   );
 };
 
